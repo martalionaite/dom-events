40a934aec7593fb4792d74ee361880881c627749
martalionaite <153713084+martalionaite@users.noreply.github.com>
2024-03-04 18:43:36 +0200

    kmi, clock, todo

--- js/header.js
+++ js/header.js
@@ -3,16 +3,20 @@ function header() {
     const localhost = 'http://localhost:4821';
     const github = 'https://github.com/martalionaite/dom-events.git';
     const href = location.hostname === 'localhost' ? localhost : github;
-   
+
     const HTML = `
-    <a href="${href}/">Home</a>
-    <a href="${href}/single-button-click">Single button click</a>
-    <a href="${href}/counter/">Counter</a>
-    <a href="${href}/basketball/">Basketball</a>
-    <a href="${href}/virykle/">Virykle</a>
-    <a href="${href}/cart/">Prekiu krepselis</a>
-`;
-navDOM.innerHTML = HTML;
+        <a href="${href}/">Home</a>
+        <a href="${href}/single-button-click">Single button click</a>
+        <a href="${href}/counter/">Counter</a>
+        <a href="${href}/basketball/">Basketball</a>
+        <a href="${href}/virykle/">Virykle</a>
+        <a href="${href}/cart/">Prekiu krepselis</a>
+        <a href="${href}/clock/">Laikrodis</a>
+        <a href="${href}/better-clock/">Geresnis laikrodis</a>
+        <a href="${href}/kmi/">KMI</a>
+        <a href="${href}/todo/">Todo</a>
+    `;
+    navDOM.innerHTML = HTML;
 }
 
 export { header };
